614ef530fedf31e26733fb62a48d8b9beee0c743
Jon Hindsgaul <jon.hindsgaul@gmail.com>
2017-12-04 13:58:21 +0100

    Updated comment for update velocity function

--- updateVel.m
+++ updateVel.m
@@ -1,6 +1,6 @@
 function [ carStruct ] = updateVel( carStruct_in )
-%UPDATEVEL Summary of this function goes here
-%   Detailed explanation goes here
+%UPDATEVEL Updates velocity of car
+
     carStruct = carStruct_in;
     carStruct.vel = carStruct.vel + carStruct.acc*carStruct.ts;
     t = carStruct.t0-carStruct.tau;
